d2c03c6657f427ef7d8bff006f3979698e969c8a
David LI <davidli4532@gmail.com>
2023-03-21 12:08:00 +0100

    functionCalculatorGood, Join AppWindowsForm Good

---
+++ WcfServiceLibrary1/WcfServiceLibrary1/Calcul.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    public class Calcul : ICalcul
+    {
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+
+            return result;
+        }
+
+
+    }
+
+}
